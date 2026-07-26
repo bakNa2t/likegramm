80f6d4b26904798bdf47a6a33b63bf5cf427dcf0
bakNa2t <ok2.lock@gmail.com>
2026-07-26 07:20:44 +0300

    Edit main page and add navbar comp

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 
+import { Navbar } from "./Navbar";
 import SessionProvider from "./SessionProvider";
 
 export const Layout = async ({ children }: { children: React.ReactNode }) => {
@@ -8,5 +9,14 @@ export const Layout = async ({ children }: { children: React.ReactNode }) => {
 
   if (!session.user) redirect("/login");
 
-  return <SessionProvider value={session}>{children}</SessionProvider>;
+  return (
+    <SessionProvider value={session}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          {children}
+        </div>
+      </div>
+    </SessionProvider>
+  );
 };
