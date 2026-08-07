87a29041f4cc5d9d38bae6279fb56a97fb9c6c61
bakNa2t <ok2.lock@gmail.com>
2026-08-07 08:00:19 +0300

    Add TrendingTopics comp

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
 import { Suspense } from "react";
+import { unstable_cache } from "next/cache";
 import { Loader2 } from "lucide-react";
+
+import { Button } from "./ui/button";
+import { UserAvatar } from "./UserAvatar";
 
 import prisma from "@/lib/prisma";
 import { validateRequest } from "@/auth";
+import { formatNumber } from "@/lib/utils";
 import { userDataSelect } from "@/lib/types";
 
-export const ToFollow = async () => {
+const ToFollow = async () => {
   const { user } = await validateRequest();
 
   if (!user) return null;
@@ -22,16 +28,96 @@ export const ToFollow = async () => {
 
   return (
     <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
-      <div className="text-xl font-bold">Who to follow</div>
+      <div className="text-xl font-bold">
+        {/* @ts-expect-error Server Component */}
+        {usersToFollow.map((user) => (
+          <div
+            key={user.id}
+            className="flex items-center justify-between gap-3"
+          >
+            <Link
+              href={`/users/${user.username}`}
+              className="flex items-center gap-3"
+            >
+              <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+              <div>
+                <p className="line-clamp-1 break-all font-semibold hover:underline">
+                  {user.displayName}
+                </p>
+                <p className="line-clamp-1 break-all text-muted-foreground">
+                  @{user.username}
+                </p>
+              </div>
+            </Link>
+
+            <Button>Follow</Button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-export const TrendsSidebar = () => {
+const getTrendingTopics = unstable_cache(
+  async () => {
+    const result = await prisma.$queryRaw<{ hashtag: string; count: bigint }[]>`
+            SELECT LOWER(unnest(regexp_matches(content, '#[[:alnum:]_]+', 'g'))) AS hashtag, COUNT(*) AS count
+            FROM posts
+            GROUP BY (hashtag)
+            ORDER BY count DESC, hashtag ASC
+            LIMIT 5
+        `;
+
+    return result.map(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //  @ts-expect-error
+      (row) => ({
+        hashtag: row.hashtag,
+        count: Number(row.count),
+      }),
+    );
+  },
+  ["trending_topics"],
+  {
+    revalidate: 3 * 60 * 60,
+  },
+);
+
+const TrendingTopics = async () => {
+  const trendingTopics = await getTrendingTopics();
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Trending topics</div>
+      {/* @ts-expect-error Server Component */}
+      {trendingTopics.map(({ hashtag, count }) => {
+        const title = hashtag.split("#")[1];
+
+        return (
+          <Link key={title} href={`/hashtag/${title}`} className="block">
+            <p
+              className="line-clamp-1 break-all font-semibold hover:underline"
+              title={hashtag}
+            >
+              {hashtag}
+            </p>
+
+            <p className="text-sm text-muted-foreground">
+              {formatNumber(count)} {count === 1 ? "post" : "posts"}
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export const TrendsSidebar = async () => {
   return (
     <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <ToFollow />
+        <TrendingTopics />
       </Suspense>
     </div>
   );
