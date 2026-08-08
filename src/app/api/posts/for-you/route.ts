7e9c12d5d334874b6fa6d367dd96e46817a95e94
bakNa2t <ok2.lock@gmail.com>
2026-08-08 07:23:12 +0300

    Add api posts for-you route

---
+++ src/app/api/posts/for-you/route.ts
@@ -0,0 +1,22 @@
+import { validateRequest } from "@/auth";
+
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
+
+export const GET = async () => {
+  try {
+    const { user } = await validateRequest();
+
+    if (!user) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const posts = await prisma.post.findMany({
+      include: postDataInclude(user.id),
+      orderBy: { createdAt: "desc" },
+    });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+};
--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -114,7 +114,7 @@ const TrendingTopics = async () => {
 
 export const TrendsSidebar = async () => {
   return (
-    <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
+    <div className="sticky top-21 hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <ToFollow />
         <TrendingTopics />
