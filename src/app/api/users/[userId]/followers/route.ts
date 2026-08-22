a6377f5d54a424bb20e6d5dd3cc4d4fc603574f8
bakNa2t <ok2.lock@gmail.com>
2026-08-22 06:59:24 +0300

    Add api GET fn followers route

---
+++ src/app/api/users/[userId]/followers/route.ts
@@ -0,0 +1,50 @@
+import { validateRequest } from "@/auth";
+
+import prisma from "@/lib/prisma";
+import { FollowerInfo } from "@/lib/types";
+
+export const GET = async (
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) => {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        followers: {
+          where: {
+            followerId: loggedInUser.id,
+          },
+          select: {
+            followerId: true,
+          },
+        },
+        _count: {
+          select: {
+            followers: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      return Response.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const data: FollowerInfo = {
+      followers: user._count.followers,
+      isFollowedByUser: !!user.followers.length,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+};
