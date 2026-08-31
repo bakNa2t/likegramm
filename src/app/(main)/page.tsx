515ba35e54f7407ed0fca222fc03fa5d7f317a68
bakNa2t <ok2.lock@gmail.com>
2026-08-31 07:35:00 +0300

    Add ForYou/FollowingFeed comps in Tabs

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,13 +2,28 @@ import { PostEditor } from "@/components/posts/editor/PostEditor";
 
 import { ForYouFeed } from "./components/ForYouFeed";
 import { TrendsSidebar } from "@/components/TrendsSidebar";
+import { FollowingFeed } from "./components/FollowingFedd";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For You</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
 
       <TrendsSidebar />
