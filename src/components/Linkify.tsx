b60aa4cd223516081d7e2ee3c1c3bd540bdae0f7
bakNa2t <ok2.lock@gmail.com>
2026-09-10 07:21:17 +0300

    Add UserLinkWithTooltip comp to Linkify

--- src/components/Linkify.tsx
+++ src/components/Linkify.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { LinkIt, LinkItUrl } from "react-linkify-it";
+import { UserLinkWithTooltip } from "./UserLinkWithTooltip";
 
 interface LinkifyProps {
   children: React.ReactNode;
@@ -35,13 +36,9 @@ const LinkifyHashtag = ({ children }: LinkifyProps) => {
     <LinkIt
       regex={/(@[a-zA-Z0-9_-]+)/}
       component={(match, key) => (
-        <Link
-          key={key}
-          href={`/hashtag/${match.slice(1)}`}
-          className="text-primary hover:underline"
-        >
+        <UserLinkWithTooltip key={key} username={match.slice(1)}>
           {match}
-        </Link>
+        </UserLinkWithTooltip>
       )}
     >
       {children}
