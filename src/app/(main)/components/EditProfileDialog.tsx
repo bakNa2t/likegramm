a2b7c7d206b54f40a7539fe1bce26326b0cfb1eb
bakNa2t <ok2.lock@gmail.com>
2026-09-17 07:24:53 +0300

    Add react Form to EditProfileDialog

--- src/app/(main)/components/EditProfileDialog.tsx
+++ src/app/(main)/components/EditProfileDialog.tsx
@@ -1,12 +1,23 @@
-import { useForm } from "react-hook-form";
+import { Form, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
   Dialog,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { LoadingButton } from "@/components/LoadingButton";
 
 import { UserData } from "@/lib/types";
 import {
@@ -36,7 +47,18 @@ export const EditProfileDialog = ({
 
   const mutation = useUpdateProfileMutation();
 
-  const onSubmit = async (values: UpdateUserProfileValues) => {};
+  const onSubmit = async (values: UpdateUserProfileValues) => {
+    mutation.mutate(
+      {
+        values,
+      },
+      {
+        onSuccess: () => {
+          onOpenChange(false);
+        },
+      },
+    );
+  };
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
@@ -44,6 +66,48 @@ export const EditProfileDialog = ({
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
         </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="displayName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Display name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your display name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="bio"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Bio</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Tell us a little bit about yourself"
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <LoadingButton type="submit" isLoading={mutation.isPending}>
+                Save
+              </LoadingButton>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
