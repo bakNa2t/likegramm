b24a96ca202a11fa88a9491e18aec8b517e8f92c
bakNa2t <ok2.lock@gmail.com>
2026-09-19 07:44:25 +0300

    Add CropImageDialog comp

--- src/app/(main)/components/EditProfileDialog.tsx
+++ src/app/(main)/components/EditProfileDialog.tsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import Image, { StaticImageData } from "next/image";
 import { Form, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Camera } from "lucide-react";
 
 import {
   Dialog,
@@ -28,46 +29,8 @@ import {
 } from "@/lib/validations";
 import { useUpdateProfileMutation } from "../users/[username]/mutaions";
 import { Label } from "@/components/ui/label";
-
-interface AvatarInputProps {
-  src: string | StaticImageData;
-  onImageCropped: (blob: Blob | null) => void;
-}
-
-const AvatarInput = ({ src, onImageCropped }: AvatarInputProps) => {
-  const [imageToCrop, setImageToCrop] = useState<File>();
-
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const onImageSelected = (image: File | undefined) => {
-    if (!image) return;
-
-    return (
-      <>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => onImageSelected(e.target.files?.[0])}
-          ref={fileInputRef}
-          className="sr-only hidden"
-        />
-        <button
-          type="button"
-          onClick={() => fileInputRef.current?.click()}
-          className="group relative block"
-        >
-          <Image
-            src={src}
-            alt="Avatar preview"
-            width={150}
-            height={150}
-            className="size-32 flex-none rounded-full object-cover"
-          />
-        </button>
-      </>
-    );
-  };
-};
+import avatarPlaceholder from "@/assets/avatar-placeholder.png";
+import { CropImageDialog } from "@/components/CropImageDialog";
 
 interface EditProfileDialogProps {
   user: UserData;
@@ -118,7 +81,7 @@ export const EditProfileDialog = ({
             src={
               croppedAvatar
                 ? URL.createObjectURL(croppedAvatar)
-                : user.avatarUrl
+                : user.avatarUrl || avatarPlaceholder
             }
             onImageCropped={setCroppedAvatar}
           />
@@ -167,5 +130,61 @@ export const EditProfileDialog = ({
         </Form>
       </DialogContent>
     </Dialog>
+  );
+};
+
+interface AvatarInputProps {
+  src: string | StaticImageData;
+  onImageCropped: (blob: Blob | null) => void;
+}
+
+const AvatarInput: React.FC<AvatarInputProps> = ({ src, onImageCropped }) => {
+  const [imageToCrop, setImageToCrop] = useState<File>();
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const onImageSelected = (image: File | undefined) => {
+    if (!image) return;
+  };
+
+  return (
+    <>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => onImageSelected(e.target.files?.[0])}
+        ref={fileInputRef}
+        className="sr-only hidden"
+      />
+      <button
+        type="button"
+        onClick={() => fileInputRef.current?.click()}
+        className="group relative block"
+      >
+        <Image
+          src={src}
+          alt="Avatar preview"
+          width={150}
+          height={150}
+          className="size-32 flex-none rounded-full object-cover"
+        />
+        <span className="absolute inset-0 m-auto flex size-12 items-center justify-center rounded-full bg-black bg-opacity-30 text-white transition-colors duration-200 group-hover:bg-opacity-25">
+          <Camera size={24} />
+        </span>
+      </button>
+      {imageToCrop && (
+        <CropImageDialog
+          src={URL.createObjectURL(imageToCrop)}
+          cropAspectRatio={1}
+          onCropped={onImageCropped}
+          onClose={() => {
+            setImageToCrop(undefined);
+            if (fileInputRef.current) {
+              fileInputRef.current.value = "";
+            }
+          }}
+        />
+      )}
+    </>
   );
 };
