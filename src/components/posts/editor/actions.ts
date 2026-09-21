5af737c6cd78874c4d5847d5add3a8efb4e1618e
bakNa2t <ok2.lock@gmail.com>
2026-09-21 07:29:41 +0300

    Add media content to schema, actions and validations

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -6,17 +6,23 @@ import prisma from "@/lib/prisma";
 import { getPostDataInclude } from "@/lib/types";
 import { createPostSchema } from "@/lib/validations";
 
-export const submitPost = async (input: string) => {
+export const submitPost = async (input: {
+  content: string;
+  mediaIds: string[];
+}) => {
   const { user } = await validateRequest();
 
   if (!user) throw new Error("Unauthorized");
 
-  const { content } = createPostSchema.parse(input);
+  const { content, mediaIds } = createPostSchema.parse(input);
 
   const newPost = await prisma.post.create({
     data: {
       content,
       userId: user.id,
+      attachments: {
+        connect: mediaIds.map((id) => ({ id })),
+      },
     },
     include: getPostDataInclude(user.id),
   });
